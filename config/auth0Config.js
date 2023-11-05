70f1e4bb193822330d1f45552aad128e4a5763ff
deepGIVAn <88539737+deepGIVAn@users.noreply.github.com>
2023-11-05 22:43:10 +0530

    updated

--- config/auth0Config.js
+++ config/auth0Config.js
@@ -1,13 +1,13 @@
 import { auth } from "express-oauth2-jwt-bearer";
 
 const jwtCheck = auth({
-    audience: process.env.APP_API,
-    issuerBaseURL: process.env.AUTH_DOMAIN,
+  // audience: process.env.APP_API,
+  // issuerBaseURL: process.env.AUTH_DOMAIN,
   //   tokenSigningAlg:"RS256",
   //   audience: "http://localhost:8000",
-  // audience: "http://localhost:8000",
-  // issuerBaseURL: "https://deepgivan.us.auth0.com",
-//   tokenSigningAlg: "RS256",
+  audience: "https://real-state-e55r.vercel.app",
+  issuerBaseURL: "https://deepgivan.us.auth0.com",
+  //   tokenSigningAlg: "RS256",
   tokenSigningAlg: "RS256",
 });
 
